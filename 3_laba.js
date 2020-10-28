fab3d57f3f9cfce9373f54baf4a5ff6196612446
Yeansis <songhoyeon16@gmail.com>
2020-10-28 20:16:09 +0700

    28.10

--- 3_laba.js
+++ 3_laba.js
@@ -1,17 +1,17 @@
 // простое изменения пользователем исходных значений
-let arr_1 = [2020, 9, 28]
-console.log( first_day_week(arr_1) )
-let arr_2 = [2020, 3.14, 28]
-console.log( first_day_week(arr_2) )
-let arr_3 = [2020, 9, "двадцать восемь"]
-console.log( first_day_week(arr_3) )
+let arr_1 = [2020, 9, 28];
+console.log( first_day_week(arr_1) );
+let arr_2 = [2020, 3.14, 28];
+console.log( first_day_week(arr_2) );
+let arr_3 = [2020, 9, "двадцать восемь"];
+console.log( first_day_week(arr_3) );
 
 
 function first_day_week(arr) {
     // проверка исходных данных на корректное значение
     for(let i of arr) {
         if(!(i % 1 === 0)) {
-            return 'Некорректные данные'
+            return 'Некорректные данные';
         }
     }
 
---
+++ 4_laba.js
@@ -0,0 +1,6 @@
+let stroka = '123456789';
+poleax_for_str(stroka);
+
+function poleax_for_str(str) {
+
+}
