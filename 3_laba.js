99afbc07dae026532ad52908d0d76288f8e6cf42
Yeansis <songhoyeon16@gmail.com>
2020-10-28 18:54:00 +0700

    28.10

--- 3_laba.js
+++ 3_laba.js
@@ -1,10 +1,17 @@
-Date.prototype.getNextWeekDay = function() {
-    let inception = this;
-    inception.setDate( this.getDate() - this.getDay() + 1 );
-    return inception;
+year = 2020;
+month = 9;
+day = 28;
+let arr = [year, month, day]
+console.log( monday_func(arr) )
+
+
+function monday_func(arr) {
+    Date.prototype.getNextWeekDay = function() {
+        let inception = this;
+        inception.setDate( this.getDate() - this.getDay() + 1 );
+        return inception.toLocaleDateString();
+    }
+
+    let now = new Date(arr[0], arr[1], arr[2]);
+    return now.getNextWeekDay();
 }
-
-
-let now = new Date();
-let nextMonday = now.getNextWeekDay();
-console.log('Next Monday : ' + nextMonday);
