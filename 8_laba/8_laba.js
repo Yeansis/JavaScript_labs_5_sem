af9d1dfa5ed5f17d536695975aba5d973b9fdf3a
Yeansis <songhoyeon16@gmail.com>
2020-11-02 15:56:08 +0700

    02.11

--- 8_laba/8_laba.js
+++ 8_laba/8_laba.js
@@ -1,15 +1,1 @@
-var data = [
-    {name:'david',id:1},
-    {name:'daniel',id:2}
-]
-
-data.forEach(function(item){
-    var option = document.createElement('option');
-    option.value = item.id;
-    option.innerHTML = item.name;
-    selector.appendChild(option)
-})
-
-selector.onchange = function(){
-    alert(this.value);
-}
+alert(('#year option').length)
