a8607e563babd8415c382f2250bcc8156c63f258
Yeansis <songhoyeon16@gmail.com>
2020-11-05 15:01:28 +0700

    05.11

--- 7_laba.js
+++ 7_laba.js
@@ -1,26 +1,32 @@
 /* 9. Напишите функцию для извлечения имен
 собственных и унаследованных свойств объекта */
+try {
+    // создание объектов
+    let vss_vintorez = {
+        use: 'combat',
+        shooting: 'silent',
+        caliber: 9,
+    };
+    let as_val = {
+        type: 'assault rifle',
+    };
 
-// создание объектов
-let vss_vintorez = {
-    use: 'combat',
-    shooting: 'silent',
-    caliber: 9,
-};
-let as_val = {
-    type: 'assault rifle',
-};
+    // прототипное наследование
+    as_val.__proto__ = vss_vintorez;
 
-// прототипное наследование
-as_val.__proto__ = vss_vintorez;
-
-// вывод полученного результата
-for(let arr of work_with_obj(as_val)){
-    console.log(arr[0], ':', arr[1]);
+    // вывод полученного результата
+    for(let arr of work_with_obj(as_val)){
+        console.log(arr[0], ':', arr[1]);
+    }
+ } catch (ex) {
+    console.log(ex.message)
 }
 
 function work_with_obj(obj) {
 
+    if (typeof(obj) !== "object") {
+        throw new Error("Ошибка!");
+    }
     /* создание словаря и запись в него
     извлеченных имен собственных ключей и
     значений унаследованных свойств */
---
+++ pes.js
@@ -0,0 +1,6 @@
+let vss_vintorez = {
+    use: 'combat',
+    shooting: 'silent',
+    caliber: 9,
+};
+console.log(typeof vss_vintorez === "object")
