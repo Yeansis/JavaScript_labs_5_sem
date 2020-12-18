e473854b03c5895f2746641bfb08f33a3460ed5e
Yeansis <songhoyeon16@gmail.com>
2020-12-18 18:31:07 +0700

    18.12

--- course_work/course_work.js
+++ course_work/course_work.js
@@ -1,14 +1,17 @@
+/* логическое отрицание выполнено при использовании только побитового оператора
+   проверка корректности вводимых данных произведена при использовании допустимых для этого операторов и конструкций */
+
 function func(){
     try{
         let val = Number(document.getElementById("value").value);
-        if(val > 2147483647 || val < -2147483648){
-            throw new SyntaxError("Неверный формат!");
-        }
+        if( val > 2147483647 || val < -2147483648 ) throw new SyntaxError("Неверный формат!");
+        else if( !((val ^ 0) === val) ) throw new SyntaxError("Введите целое число!");
         else{
             document.getElementById("text_spec_value_10").innerHTML = "Заданное число в 2 виде:";
             document.getElementById("spec_value_10").innerHTML = val;
             document.getElementById("text_spec_value_2").innerHTML = "Заданное число в 2 виде:";
             document.getElementById("spec_value_2").innerHTML = (val>>>0).toString(2);
+            //логическое отрицание
             val = ~val;
             document.getElementById("text").innerHTML = "Вычислена инверсия заданного числа:";
             document.getElementById("text_res_value_10").innerHTML = "Результат в 10 виде:";
@@ -18,7 +21,7 @@ function func(){
         }
     }
     catch(ex){
-        alert(ex.message)
+        alert( ex.message )
         document.getElementById("text for specified value").innerHTML = "";
         document.getElementById("specified value in 2 form").innerHTML = "";
         document.getElementById("text").innerHTML = "";
