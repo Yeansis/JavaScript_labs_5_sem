a0c287c53d1c6c7923241f349a62d0a6dd43e7db
Yeansis <songhoyeon16@gmail.com>
2020-12-18 18:16:05 +0700

    18.12

--- course_work/course_work.js
+++ course_work/course_work.js
@@ -5,14 +5,16 @@ function func(){
             throw new SyntaxError("Неверный формат!");
         }
         else{
-            document.getElementById("text for specified value").innerHTML = "Заданное число в 2 виде:"
-            document.getElementById("specified value in 2 form").innerHTML = (val>>>0).toString(2);
-            val = ~val
-            document.getElementById("text").innerHTML = "Вычислена инверсия заданного числа:"
-            document.getElementById("text result in 10 form").innerHTML = "Результат в 10 виде:"
-            document.getElementById("result in 10 form").innerHTML = val;
-            document.getElementById("text result in 2 form").innerHTML = "Результат в 2 виде:"
-            document.getElementById("result in 2 form").innerHTML = val;
+            document.getElementById("text_spec_value_10").innerHTML = "Заданное число в 2 виде:";
+            document.getElementById("spec_value_10").innerHTML = val;
+            document.getElementById("text_spec_value_2").innerHTML = "Заданное число в 2 виде:";
+            document.getElementById("spec_value_2").innerHTML = (val>>>0).toString(2);
+            val = ~val;
+            document.getElementById("text").innerHTML = "Вычислена инверсия заданного числа:";
+            document.getElementById("text_res_value_10").innerHTML = "Результат в 10 виде:";
+            document.getElementById("res_value_10").innerHTML = val;
+            document.getElementById("text_res_value_2").innerHTML = "Результат в 2 виде:";
+            document.getElementById("res_value_2").innerHTML = (val>>>0).toString(2);
         }
     }
     catch(ex){
